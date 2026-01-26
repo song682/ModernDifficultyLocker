8be1beefde6ad9aeddc66b9e2f13e415c588676c
dfdvdsf <81430928+song682@users.noreply.github.com>
2026-01-26 23:15:31 +0800

    Create DifficultyLocker.java
    Implement DifficultyLocker mod with initialization handlers

---
+++ src/main/java/decok/dfcdvadstf/difficultyLocker/DifficultyLocker.java
@@ -0,0 +1,23 @@
+package decok.dfcdvadstf.difficultyLocker;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = Tags.MODID, name = Tags.NAME, version = Tags.VERSION, acceptedMinecraftVersions = Tags.ACCEPTED_MC_VERSION, acceptableRemoteVersions = Tags.ACCEPTED_MC_VERSION)
+public class DifficultyLocker {
+    public static Logger logger;
+
+    @EventHandler
+    public void preInit (FMLPreInitializationEvent event){
+        logger = event.getModLog();
+        logger.info("Difficulty Locker is preinitializing.....");
+    }
+
+    @EventHandler
+    public void init (FMLInitializationEvent event){
+        logger.info("Difficulty Locker is initializing");
+    }
+}
